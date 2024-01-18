b8aa42ba58732537aeac387a76fafd64628e41f9
DylanCa <dylan.cattelan@gmail.com>
2024-01-18 15:19:07 +0100

    Updated main.rs to handle server and import routes from crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use hello_world;
+use user;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .merge(hello_world::routes::get_routes())
+        .merge(users::routes::get_routes());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }

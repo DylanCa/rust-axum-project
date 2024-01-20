4f74ebbbef6b16628dd579acdc1b026a50937117
DylanCa <dylan.cattelan@gmail.com>
2024-01-20 15:47:17 +0100

    Added DB pool logic to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
 use axum::Router;
+use dotenv::dotenv;
+use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{MySql, MySqlPool, Pool};
+use std::sync::Arc;
+
 mod api;
+
+#[derive(Debug)]
+pub struct AppState {
+    db: MySqlPool,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .nest("/api", api::routes::get_routes());
+    dotenv().ok();
+    println!("🌟 Rust Playground Server 🌟");
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let pool = get_db_pool().await;
+    let app_state = Arc::new(AppState { db: pool.clone() });
 
-    axum::serve(listener, app).await.unwrap();
+    let router = Router::new().nest("/api", api::routes::get_routes(app_state.clone()));
+
+    let serv_addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(&serv_addr).await.unwrap();
+    println!("✅ Server started successfully at {serv_addr}");
+
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn get_db_pool() -> Pool<MySql> {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must set");
+    let pool = match MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅ Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("❌ Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    pool
 }

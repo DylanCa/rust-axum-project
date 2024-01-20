cbbad2e73f11e0cdf6454817215bbfca847dd85a
DylanCa <dylan.cattelan@gmail.com>
2024-01-20 13:50:34 +0100

    Nested api modules in router in main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod api;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .merge(api::routes::get_routes());
+        .nest("/api", api::routes::get_routes());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 

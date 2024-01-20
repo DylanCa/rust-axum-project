e63349d712ff425c2f8e7c4f6a3ac5b44f44df2f
DylanCa <dylan.cattelan@gmail.com>
2024-01-20 15:49:09 +0100

    Modified create_user and get_user methods to use MySQL DB

--- src/api/users/handlers.rs
+++ src/api/users/handlers.rs
@@ -1,41 +1,73 @@
-use axum::Json;
+use crate::api::users::models::{User, UserResponse};
+use crate::AppState;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
-use serde::{Deserialize, Serialize};
+use axum::Json;
+use serde_json::json;
+use std::sync::Arc;
 
 pub async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
+    State(data): State<Arc<AppState>>,
+    Json(payload): Json<User>,
+) -> Result<(StatusCode, Json<User>), (StatusCode, Json<serde_json::Value>)> {
+    let user = User::new(payload);
+
+    let query_result = sqlx::query(r#"INSERT INTO users (id, name, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)"#)
+        .bind(user.id().clone())
+        .bind(user.name.clone())
+        .bind(user.email().clone())
+        .bind(user.password_hash().clone())
+        .bind(user.created_at().clone())
+        .bind(user.updated_at().clone())
+        .execute(&data.db)
+        .await
+        .map_err(|err: sqlx::Error| err.to_string());
+
+    println!("{:#?}", query_result);
+
+    if let Err(err) = query_result {
+        if err.contains("Duplicate entry") {
+            let error_response = serde_json::json!({
+                "status": "error",
+                "message": "User already exists",
+            });
+            return Err((StatusCode::CONFLICT, Json(error_response)));
+        }
+
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"status": "error","message": format!("{:?}", err)})),
+        ));
+    }
+
+    Ok((StatusCode::CREATED, Json(user)))
+}
+
+pub async fn get_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Path(uuid): Path<uuid::Uuid>,
+    State(data): State<Arc<AppState>>,
+) -> Result<(StatusCode, Json<UserResponse>), (StatusCode, Json<serde_json::Value>)> {
+    let query_result = sqlx::query_as!(
+        UserResponse,
+        r#"SELECT * FROM users WHERE id = ?"#,
+        uuid.to_string()
+    )
+    .fetch_one(&data.db)
+    .await
+    .map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"status": "error","message": format!("{:?}", e)})),
+        )
+    })?;
+
+    println!("{:#?}", query_result);
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-pub async fn get_user() -> (StatusCode, Json<User>) {
-    let user = User {
-        id: 1337,
-        username: String::from("Bob"),
-    };
-
-    (StatusCode::OK, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-pub struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-pub struct User {
-    id: u64,
-    username: String,
+    Ok((StatusCode::CREATED, Json(query_result)))
 }

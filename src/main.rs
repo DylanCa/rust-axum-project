9982d4ca80ca27b99b68b87d4cd75728d1c603c8
DylanCa <dylan.cattelan@gmail.com>
2024-01-20 13:34:48 +0100

    Modified main.rs to use api module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use axum::Router;
-use hello_world;
-use user;
+mod api;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .merge(hello_world::routes::get_routes())
-        .merge(users::routes::get_routes());
+        .merge(api::routes::get_routes());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 

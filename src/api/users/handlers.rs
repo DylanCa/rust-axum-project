6bb6fc847874a371f874dc75ff00dbf2c2719bcd
DylanCa <dylan.cattelan@gmail.com>
2024-01-21 13:27:20 +0100

    Added Cookie logic to login handler

--- src/api/users/handlers.rs
+++ src/api/users/handlers.rs
@@ -1,15 +1,15 @@
 use crate::api::users::models::{User, UserLogin, UserResponse};
+use crate::api::AUTH_TOKEN;
 use crate::AppState;
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::Json;
+use bcrypt::verify;
 use serde_json::json;
 use std::sync::Arc;
-use bcrypt::verify;
+use tower_cookies::{Cookie, Cookies};
 
 pub async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
     State(state): State<Arc<AppState>>,
     Json(payload): Json<User>,
 ) -> Result<(StatusCode, Json<User>), (StatusCode, Json<serde_json::Value>)> {
@@ -47,8 +47,6 @@ pub async fn create_user(
 }
 
 pub async fn get_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
     Path(uuid): Path<uuid::Uuid>,
     State(state): State<Arc<AppState>>,
 ) -> Result<(StatusCode, Json<UserResponse>), (StatusCode, Json<serde_json::Value>)> {
@@ -68,35 +66,40 @@ pub async fn get_user(
 
     println!("{:#?}", query_result);
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
     Ok((StatusCode::CREATED, Json(query_result)))
 }
 
 pub async fn login(
+    cookies: Cookies,
     State(data): State<Arc<AppState>>,
     Json(payload): Json<UserLogin>,
 ) -> Result<StatusCode, (StatusCode, Json<serde_json::Value>)> {
-    let query_result = sqlx::query_as!(
+    let user = sqlx::query_as!(
         UserResponse,
         r#"SELECT * FROM users WHERE email = ?"#,
         payload.email
     )
-        .fetch_one(&data.db)
-        .await
-        .map_err(|e| {
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(json!({"status": "error","message": format!("{:?}", e)})),
-            )
-        })?;
+    .fetch_one(&data.db)
+    .await
+    .map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"status": "error","message": format!("{:?}", e)})),
+        )
+    })?;
 
-    if !verify(payload.password, &*query_result.password).unwrap() {
-        return Err(
-            (StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({"status": "error","message": "Wrong Password."})),)
-        );
+    if !verify(payload.password, &*user.password).unwrap() {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"status": "error","message": "Wrong Password."})),
+        ));
     }
+
+    // FIXME: Implement a auth-token generation / exp / signature
+    cookies.add(Cookie::new(
+        AUTH_TOKEN,
+        format!("{}.{}.{}", user.id, "exp", "sign"),
+    ));
 
     Ok(StatusCode::OK)
 }

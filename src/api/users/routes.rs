fb64200e3d55643161cb19ca626acfbb525b3c8c
DylanCa <dylan.cattelan@gmail.com>
2024-01-21 12:52:40 +0100

    Added login route

--- src/api/users/routes.rs
+++ src/api/users/routes.rs
@@ -1,4 +1,4 @@
-use crate::api::users::handlers::{create_user, get_user};
+use crate::api::users::handlers::{create_user, get_user, login};
 use crate::AppState;
 use axum::routing::{get, post};
 use axum::Router;
@@ -6,6 +6,7 @@ use std::sync::Arc;
 
 pub fn get_routes(app_state: Arc<AppState>) -> Router {
     Router::new()
+        .route("/login", post(login))
         .route("/users/:uuid", get(get_user))
         .route("/users", post(create_user))
         .with_state(app_state)

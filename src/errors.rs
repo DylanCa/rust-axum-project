277985d0344a2fb79948100c0a02f3cd26a43671
DylanCa <dylan.cattelan@gmail.com>
2024-01-25 19:47:59 +0100

    Improved errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,7 @@
 use axum::http::StatusCode;
+use axum::Json;
 use axum::response::{IntoResponse, Response};
+use serde_json::json;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
@@ -16,6 +18,13 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("{self:#?}");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        let mut response = (StatusCode::INTERNAL_SERVER_ERROR, false, "UNHANDLED_CLIENT_ERROR");
+        match self {
+            Error::LoginFailed | Error::WrongPassword | Error::UserNotFound | Error::AuthFailNoAuthTokenCookie | Error::AuthFailTokenWrongFormat => response = (StatusCode::UNAUTHORIZED, false, "LOGIN_FAILED")
+        }
+
+        let status_code = response.0;
+        let body = json!({"response": {"success": response.1, "message": response.2}});
+        (status_code, Json(body)).into_response()
     }
 }

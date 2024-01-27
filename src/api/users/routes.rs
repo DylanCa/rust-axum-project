2b9b777de8c49fe6fc38811453f9112d3e75a6df
DylanCa <dylan.cattelan@gmail.com>
2024-01-27 13:39:31 +0100

    Moved user creation route out of authed API -> to /signup route

--- src/api/users/routes.rs
+++ src/api/users/routes.rs
@@ -7,12 +7,12 @@ use std::sync::Arc;
 pub fn get_routes(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/user", get(get_user))
-        .route("/users", post(create_user))
         .with_state(app_state)
 }
 
 pub fn get_login(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/login", post(login))
+        .route("/signup", post(create_user))
         .with_state(app_state)
 }

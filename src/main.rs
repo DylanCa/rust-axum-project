975b0f8c0b1f3923aa326adabc0c3ccc29a86b0f
DylanCa <dylan.cattelan@gmail.com>
2024-01-27 13:29:36 +0100

    Added on_connect method to handle socket messages in main.rs

--- src/main.rs
+++ src/main.rs
@@ -77,3 +77,24 @@ async fn main_response_mapper(res: Response) -> Response {
 
     res
 }
+
+fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
+
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+           socket.bin(bin).emit("message-back", data).ok();
+        }
+    );
+
+    socket.on(
+        "message-with-ack",
+        |Data::<Value>(data), ack: AckSender, Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            ack.bin(bin).send(data).ok();
+        }
+    );
+}

813518a6b11ee21c33a0bec9d86ed4b4cd93f26a
DylanCa <dylan.cattelan@gmail.com>
2024-01-28 17:16:59 +0100

    Moved on_connect handler to chat/handlers.rs

---
+++ src/api/chat/handlers.rs
@@ -0,0 +1,24 @@
+use log::info;
+use serde_json::Value;
+use socketioxide::extract::{AckSender, Bin, Data, SocketRef};
+
+pub fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
+
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            socket.bin(bin).emit("message-back", data).ok();
+        },
+    );
+
+    socket.on(
+        "message-with-ack",
+        |Data::<Value>(data), ack: AckSender, Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            ack.bin(bin).send(data).ok();
+        },
+    );
+}

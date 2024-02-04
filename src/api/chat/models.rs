1e81369d98f317564985c6bb199d45dbd8d36630
DylanCa <dylan.cattelan@gmail.com>
2024-02-04 18:35:31 +0100

    Added Chatroom model to api/chat/models

--- src/api/chat/models.rs
+++ src/api/chat/models.rs
@@ -10,4 +10,19 @@ pub struct OutMessage {
     pub user_id: String,
     pub message: String,
     pub datetime: chrono::DateTime<chrono::Utc>,
+#[derive(Debug, sqlx::FromRow)]
+pub struct Chatroom {
+    id: String,
+    name: String,
+    created_at: DateTime<Utc>,
+}
+impl Chatroom {
+    pub fn new(name: String) -> Self {
+        Self {
+            id: uuid::Uuid::new_v4().to_string(),
+            name,
+            created_at: chrono::Utc::now(),
+        }
+    }
+}
 }

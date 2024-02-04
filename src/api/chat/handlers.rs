fc7c9ac903a119501b11acd4623282aa00ed854e
DylanCa <dylan.cattelan@gmail.com>
2024-02-04 18:37:31 +0100

    Reworked chat/handlers to use IncomingMessage & ChatMessage instead

--- src/api/chat/handlers.rs
+++ src/api/chat/handlers.rs
@@ -1,7 +1,7 @@
-use crate::api::chat::models::{InMessage, OutMessage};
-use log::info;
 use serde_json::Value;
-use socketioxide::extract::{AckSender, Bin, Data, SocketRef};
+use socketioxide::extract::{Bin, Data, SocketRef};
+use tracing::{info};
+use crate::api::chat::models::{ChatMessage, IncomingMessage};
 
 pub fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
     info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
@@ -15,16 +15,11 @@ pub fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
 
     socket.on(
         "message",
-        |socket: SocketRef, Data::<InMessage>(mut data), Bin(bin)| {
+        |socket: SocketRef, Data::<IncomingMessage>(data), Bin(bin)| {
             info!("Received event: {:?} {:?}", data, bin);
 
-            let response = OutMessage {
-                user_id: format!("user-{}", socket.id),
-                datetime: chrono::Utc::now(),
-                message: data.message,
-            };
-
-            let _ = socket.within(data.room).emit("message", response);
+            let response = ChatMessage::new(data.room_id, socket.id.to_string(), data.message);
+            let _ = socket.within(response.clone().room_id()).emit("message", response);
         },
     );
 }
